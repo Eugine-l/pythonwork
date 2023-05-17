6eb810e00cde1434f8213ab696f687f1bac3aa20
Eugine-l <122367548+Eugine-l@users.noreply.github.com>
2023-05-17 10:29:01 +0300

    Add files via upload
    This is my first python program.

---
+++ Pythoncls1.py
@@ -0,0 +1,5 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+print("Welcome \nto \n Python programming")
+print("Best\n programming \n language")
