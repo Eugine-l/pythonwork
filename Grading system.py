376e073d92d40072afae10a959e0f16d3dbaa8df
Eugine-l <122367548+Eugine-l@users.noreply.github.com>
2023-06-05 13:40:41 +0300

    Add files via upload

---
+++ Grading system.py
@@ -0,0 +1,11 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 =int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# Display the result
+print("The average of the three numbers is:", average)
+
